0b4bcc66220ed9f06b6d00159952aff747843b81
Nasser Fathi <nas7.5@hotmail.com>
2022-04-12 16:35:31 +0200

    simple test program to test the alarm and the buttons

--- software/test_main.py
+++ software/test_main.py
@@ -1,12 +1,39 @@
 from alarm import *
 from buttons import *
+import time
 
+try:
+	from i2cDisplay import *
+	from hardware_init import *
+except:
+	from spiDisplay import *
 
-alarm_b = Alarm(15)
-alarm_b.init()
-alarm_b.enable()
-alarm_b.disable()
+alarm = Alarm(26)
+alarm.init()
 
-buttons = Buttons(1,2,3,4)
+buttons = Buttons(12,16,20,21)
 buttons.init()
-buttons.poll()
+
+
+alarm.enable()
+time.sleep(2)
+alarm.disable()
+time.sleep(0.5)
+alarm.enable()
+time.sleep(0.5)
+alarm.disable()
+time.sleep(0.5)
+alarm.enable()
+time.sleep(0.5)
+alarm.disable()
+
+while True:
+	status = buttons.poll()
+	#write(0, 0, "Button up: " + str(status["UP"]))
+	#write(0, 1, "Button down: " + str(status["DOWN"]))
+	#write(0, 2, "Button left: " + str(status["LEFT"]))
+	write(0, 3, "Button right: " + str(status["RIGHT"]))
+	
+	#TODO: FIX write() bug (Every write() call will clear the whole display, which makes writing to multiple lines impossible)
+	
+	
