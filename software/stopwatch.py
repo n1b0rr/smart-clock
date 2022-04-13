ea5aaa05ec4de29ba7261aade1bac6643ba5974c
n1b0rr <robin.schellemans174@gmail.com>
2022-04-13 12:58:31 +0200

    added buttons to the stopwatch

--- software/stopwatch.py
+++ software/stopwatch.py
@@ -1,14 +1,40 @@
+from buttons import *
+
 try:
 	from i2cDisplay import *
+
 except:
 	from spiDisplay import *
 
 import time
 
+buttons = Buttons(12,16,20,21)
+buttons.init()
+stopwatch_active = False
+backup = 0
+interval = 0
 
-start = time.time()
 while True:
-    interval = round(time.time() - start)
+    status = buttons.poll()
+
+# reset stopwatch
+    if(status["DOWN"]):
+        backup = 0
+        interval = 0
+        start = time.time()
+
+# start or stop the stopwatch 
+    if(status["UP"]):
+        if(stopwatch_active):
+            stopwatch_active = False
+            backup += round(time.time() - start)
+        else:
+            stopwatch_active = True
+            start = time.time()
+
+    if(stopwatch_active):
+        interval = backup + round(time.time() - start)
+
     hours = int(interval/3600)
     minutes = int((interval - (hours*3600))/60)
     seconds = int(interval - (hours*3600) - (minutes*60))
