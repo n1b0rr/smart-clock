50fea9be9c405d1b3b32ce9c25a3876767a1df30
n1b0rr <robin.schellemans174@gmail.com>
2022-04-22 13:45:45 +0200

    stats script to show ip address on display

---
+++ software/stats.py
@@ -0,0 +1,41 @@
+import os
+from datetime import datetime
+import subprocess
+
+# libraries for oled display
+from board import SCL, SDA
+import busio
+import adafruit_ssd1306
+
+# library for making images, to display
+from PIL import Image, ImageDraw, ImageFont, GifImagePlugin
+
+i2c = busio.I2C(SCL,SDA)
+
+disp = adafruit_ssd1306.SSD1306_I2C(128,64,i2c)
+font = ImageFont.truetype("/home/pi/smart-clock/software/fonts/liberation.ttf", size=10)
+
+disp.fill(0)
+disp.show()
+
+width = disp.width
+height = disp.height
+
+# make an empty image on which you can write things on,
+#this image will be displayed at a continious rate per second
+image = Image.new("1", (width, height), color=0)
+
+
+
+def write(x, y, ctx):
+    draw = ImageDraw.Draw(image, "1")
+    draw.rectangle((width, height,0,0), fill=(0))
+    draw.text((x, y), ctx, font=font, fill=255)
+    disp.image(image)
+    disp.show()
+
+while True:
+    IP = subprocess.check_output('hostname -I', shell=True) 
+    write(0,0, str(IP))
+
+
